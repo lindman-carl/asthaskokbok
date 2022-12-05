a6ccfae8c57b1b0651428a5b6c677579700d5a6e
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-12-05 15:53:04 +0100

    add markdown support for recipe content

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Recipe } from "@prisma/client";
+import markdownToHtml from "../utils/markdownToHtml";
 
 type RecipeProps = {
   recipe: Recipe;
 };
 
 const Recipe: React.FC<RecipeProps> = ({ recipe: { title, content } }) => {
+  const [htmlContent, setHtmlContent] = useState<string>("");
+  useEffect(() => {
+    const convertContent = async () => {
+      const convertedContent = await markdownToHtml(content);
+      setHtmlContent(convertedContent);
+    };
+
+    convertContent();
+  }, [content]);
+
   return (
     <div>
       <h3>{title}</h3>
-      <p>{content}</p>
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </div>
   );
 };
